babf76257b4c82e237fab802932e4d7dc482f5d7
angeloquenna <89348788+mindexpert7546@users.noreply.github.com>
2023-11-03 18:03:30 +0530

    first commit

---
+++ grails-app/domain/com/mindexpert/Countries.groovy
@@ -0,0 +1,12 @@
+package com.mindexpert
+
+class Countries {
+    String countryName
+    String countryCode
+    byte[] flag
+    static constraints = {
+        countryName nullable:false 
+        countryName nullable: false
+        flag nullable: true
+    }
+}
